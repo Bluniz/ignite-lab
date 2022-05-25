1723b42bfda239c9e74cf6e7e5e22a7174832ef9
Lucas Rosa <lucassantosrosa51@gmail.com>
2022-05-25 17:25:32 -0300

    Add aula 04 - Configurando frontend e integrando com o backend

--- web/src/pages/app/index.tsx
+++ web/src/pages/app/index.tsx
@@ -1,16 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
 import { getSession, useUser, withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { GetServerSideProps } from "next";
+import {
+  getServerPageGetProducts,
+  ssrGetProducts,
+} from "../../graphql/generated/page";
+import { withApollo } from "../../lib/withApollo";
 
-export default function Home() {
+function Home({ data }) {
   const user = useUser();
+
+  //const { data, loading, error } = useQuery(PRODUCTS_QUERY);
 
   return (
     <div>
-      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <div>
+        <pre>{JSON.stringify(data.products, null, 2)}</pre>
 
-      <a href="api/auth/logout">Logout</a>
+        <pre>{JSON.stringify(user, null, 2)}</pre>
+
+        <a href="api/auth/logout">Logout</a>
+      </div>
     </div>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = withPageAuthRequired();
+export const getServerSideProps: GetServerSideProps = withPageAuthRequired({
+  getServerSideProps: async (ctx) => {
+    //const data = await getServerPageGetProducts(null, ctx);
+
+    return getServerPageGetProducts(null, ctx);
+  },
+});
+
+export default withApollo(ssrGetProducts.withPage()(Home));
